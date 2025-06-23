7df0d562a1e16a7ee571add6bc0c9bb0240ba780
antoineleducmusic <antoineleducmusic@gmail.com>
2025-06-23 16:39:20 -0400

    Add files via upload

---
+++ gradlew
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Simulated gradlew wrapper
+./gradle/wrapper/gradle-wrapper.jar "$@"
---
+++ gradlew.bat
@@ -0,0 +1,3 @@
+@echo off
+REM Simulated gradlew wrapper
+java -jar gradle\wrapper\gradle-wrapper.jar %*
